3ef13910c057c367f0009fe55fc773d5ddf6f082
java <tyler4077@icloud.com>
2026-01-24 17:48:52 -0500

    fix?

--- conanfile.py
+++ conanfile.py
@@ -4,14 +4,20 @@ from conan.tools.cmake import cmake_layout
 class OrbitalSimConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
-    
+
     def requirements(self):
         self.requires("sdl/3.2.20")
         self.requires("cjson/1.7.19")
         self.requires("opengl/system")
-        
+
         if self.settings.os != "Emscripten":
             self.requires("glew/2.2.0")
-    
+
+    def configure(self):
+        # Build SDL3 as shared library on Windows to avoid naming issues
+        # (static builds produce SDL3-static.lib which CMakeDeps can't find)
+        if self.settings.os == "Windows":
+            self.options["sdl"].shared = True
+
     def layout(self):
         cmake_layout(self)
